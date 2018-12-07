227327798fa0fbbada37ca32ff42691d07a35a9e
abutoyibaz <muhammadazis801@gmail.com>
2018-12-07 19:08:37 +0700

    commit 7-12-2018

---
+++ index.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="icon" href="assets/img/favicon.png">
+
+    <title>The Trailer Pro</title>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg warna-navbar">
+        <a class="navbar-brand text-warning" href="#">
+            <img src="assets/img/favicon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            <b>Trailexer</b>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon text-white"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ml-4">
+            <li class="nav-item active">
+                <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Synopsis</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Trailers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">About</a>
+            </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+        </nav>
+
+        <section>
+            <!-- header/jumbotron -->
+            <div class="slider marginatasfix">
+                <h3>COLLECTIONS OF SYNOPSIS & TRAILER</h3>
+                <button type="button" class="btn btn-warning btn-lg tombol-header">SEE OUR COLLECTIONS</button>
+            </div>
+            <!-- akhir header/jumbotron -->
+        </section>
+
+        <!-- Popular Synopsis -->
+        <section class="lebar-sinopsis warna-navbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="text-center text-white mt-3"><b>Most Popular Synopsis</b></h2>
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-md-4 mt-4">
+                        <center>
+                            <img class="shadow-img mt-4" src="assets/img/film.jpg" alt="San Andreas" width="300">
+                        </center>
+                    </div>
+                    <div class="col-md-8 mt-4 ml-12">
+                        <h3 class="text-white text-center">San Andreas</h3>
+                        <p class="text-white ml-6 float-right">
+                        San Andreas  bercerita mengenai kisah seorang ayah yang sangat dekat dengan putrinya yang bernama yang Blake (Alexandra Daddario). Disaat terjadi bencana gempa bumi dahsyat yang  melanda kota California, Ray (Dwayne Johnson) yang berprofesi sebagai pilot helikopter di departemen penyelamatan ini langsung teringat mengenai putrinya yang tinggal terpisah dari mantan istrinya, Emma (Carla Gugino). Sang gadis yang sudah beranjak dewasa ini diketahui sedang berada di sebuah gedung perkantoran bertingkat.<br><br>
+                        Dengan dihantui rasa cemas Ray menerjang bahaya menelusuri setiap negara bagian berusaha untuk mencari putrinya yang sedang bertahan untuk tetap hidup dan menyematkan para korban gempa lainnya yang terisolir diantara reruntuhan tersebut, sang mantan istri Emma pun turut mencari Blake. Saat itu kota California berubah menjadi kota yang seolaha<a href="">...Baca Selengkapnya</a>
+                        </p>
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-md-12">
+                        <button type="button" class="btn btn-outline-warning float-right">See Another Synopsis &gt;&gt;</button>
+                    </div>
+                </div> 
+            </div>
+        </section>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="assets/img/slide-1.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slide-2.png" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="assets/img/slide-3.png" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+
+        <section class="warna-navbar lebar-embed">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="text-center text-white mt-4 mb-4">Top Video Trailer</h3>
+                    </div> 
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe src="https://www.youtube.com/embed/MlWSX6L6zec" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-12">
+                        <button type="button" class="btn btn-outline-warning float-right">See Another Video Trailer &gt;&gt;</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="bg-dark section-footer">
+            <div class="container">
+                <div class="row text-center text-white">
+                    <div class="col-md-12">
+                        <h1>About Us</h1>
+                        <p class="mt-5">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolor animi nobis necessitatibus dolore amet quo commodi recusandae! Debitis cum porro aliquam in, pariatur blanditiis eum ipsa natus magnam. Incidunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci doloribus ipsa dignissimos quia quaerat molestiae et ut laboriosam ratione animi iste optio, veritatis nihil amet cupiditate a natus vitae! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem non a odit veritatis nam quae animi eos recusandae doloremque aut, id enim? Quod sit minima unde nemo temporibus in sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis quidem, perferendis nam velit porro, harum rerum, ipsa ut possimus aperiam eaque assumenda eum ducimus ab fuga. Est magnam velit laudantium!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="bg-dark tinggi-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center text-white">&copy; Copyright 2018 Designed By <a href="">Abu T.A</a></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script>
+        $('.carousel').carousel({
+        interval: 50
+        })
+    </script>
+  </body>
+</html>
