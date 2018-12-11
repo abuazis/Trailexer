8d43232391c011a411cdf317bd56af1f1d547013
abutoyibaz <muhammadazis801@gmail.com>
2018-12-11 18:49:17 +0700

    11-12-2018

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <link rel="icon" href="assets/img/favicon.png">
 
@@ -22,7 +24,7 @@
             <b>Trailexer</b>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon text-white"></span>
+        <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
@@ -77,7 +79,7 @@
                         <h3 class="text-white text-center">San Andreas</h3>
                         <p class="text-white ml-6 float-right">
                         San Andreas  bercerita mengenai kisah seorang ayah yang sangat dekat dengan putrinya yang bernama yang Blake (Alexandra Daddario). Disaat terjadi bencana gempa bumi dahsyat yang  melanda kota California, Ray (Dwayne Johnson) yang berprofesi sebagai pilot helikopter di departemen penyelamatan ini langsung teringat mengenai putrinya yang tinggal terpisah dari mantan istrinya, Emma (Carla Gugino). Sang gadis yang sudah beranjak dewasa ini diketahui sedang berada di sebuah gedung perkantoran bertingkat.<br><br>
-                        Dengan dihantui rasa cemas Ray menerjang bahaya menelusuri setiap negara bagian berusaha untuk mencari putrinya yang sedang bertahan untuk tetap hidup dan menyematkan para korban gempa lainnya yang terisolir diantara reruntuhan tersebut, sang mantan istri Emma pun turut mencari Blake. Saat itu kota California berubah menjadi kota yang seolaha<a href="">...Baca Selengkapnya</a>
+                        Dengan dihantui rasa cemas Ray menerjang bahaya menelusuri setiap negara bagian berusaha untuk mencari putrinya yang sedang bertahan untuk tetap hidup dan menyematkan para korban gempa lainnya yang terisolir diantara reruntuhan tersebut, sang mantan istri Emma pun turut mencari Blake. Saat itu kota California berubah menjadi kota yang seolaha<a href="view/synopsis.php">...Baca Selengkapnya</a>
                         </p>
                     </div>
                 </div> 
@@ -173,14 +175,31 @@
             </div>
         </footer>
 
+        <button id="topBtn"><i class="fas fa-arrow-up"></i></button>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="assets/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(window).scroll(function(){
+                if($(this).scrollTop() > 40){
+                    $('#topBtn').fadeIn();
+                } else {
+                    $('#topBtn').fadeOut();
+                }
+            });
+
+            $("#topBtn").click(function(){
+                $('html, body').animate({scrollTop : 0},3000);
+            });
+        });
+    </script>
     <script>
         $('.carousel').carousel({
-        interval: 50
+        interval: 2000
         })
     </script>
   </body>
---
+++ view/synopsis.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <link rel="icon" href="../assets/img/favicon.png">
+
+    <title>The Trailer Pro</title>
+  </head>
+  <body>
+
+    <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg warna-navbar">
+        <a class="navbar-brand text-warning" href="#">
+            <img src="../assets/img/favicon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            <b>Trailexer</b>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ml-4">
+            <li class="nav-item active">
+                <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Synopsis</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Trailers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Gallery</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">About</a>
+            </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+        </nav>
+
+        <section class="bg-sinopsis lebar-sinopsis ">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="text-center text-white mt-3 text-dark"><b>San Andreas (2015)</b></h2>
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-md-4 mt-4">
+                        <center>
+                            <img class="shadow-img mt-4" src="../assets/img/film.jpg" alt="San Andreas" width="300">
+                        </center>
+                    </div>
+                    <div class="col-md-8 mt-4 ml-12">
+                        <h3 class="text-white text-center text-dark">San Andreas</h3>
+                        <p class="text-white ml-6 float-right text-dark">
+                        » Movie Tayang: Mei-Juni 2015<br>
+                        » Movie Theaters: 29 Mei 2015<br>
+                        » Genre: Action, Drama, Thriller<br>
+                        » Writer: Carlton Cuse, Carey Hayes, Chad Hayes<br>
+                        » Director: Brad Peyton<br>
+                        » Companies: Warner Bros. Pictures<br>
+                        » Official from: variety.com<br>
+                        » MPAA Rating: R<br>
+                        » Cast: Dwayne Johnson, Art Parkinson, Carla Gugino, Alexandra Daddario, Will Yun Lee, Ioan Gruffudd<br><br>
+                        Film "San Andreas" ini akan menceritakan sebuah bencana gempa bumi terjadi di California, Los Angeles. Ray (Dwayne Johnson) adalah seorang pilot helikopter di departemen penyelamatan. Dia memiliki seorang putri bernama Blake (Alexandra Daddario) yang sudah dewasa dan tinggal terpisah dari mantan istrinya, Emma (Carla Gugino). Setelah gempa bumi terjadi keadaan California menjadi hancur dan tidak ada tempat aman yang dapat ditinggali. Ray bersama Emma berusaha melakukan penyelamatan untuk putri mereka yang juga berjuang bertahan hidup.
+
+                        Itu dia tadi sedikit info Movie Terbaru Action Adventure San Andreas (2015) Rilis Teather 21 Internasional (USA) mulai 29 Mei 2015. Ditunggu Film San Andreas 3D di Bioskop 2015 Cinema 21-XXI - BlitzMegaplex atau Bioskop Lokal (Non-Cineplex) lainnya yang kemungkinan tayang Mei-Juni 2015.
+                        </p>
+                    </div>
+                </div> 
+            </div>
+        </section>
+
+
+
+        <section class="bg-dark section-footer">
+            <div class="container">
+                <div class="row text-center text-white">
+                    <div class="col-md-12">
+                        <h1>About Us</h1>
+                        <p class="mt-5">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt dolor animi nobis necessitatibus dolore amet quo commodi recusandae! Debitis cum porro aliquam in, pariatur blanditiis eum ipsa natus magnam. Incidunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci doloribus ipsa dignissimos quia quaerat molestiae et ut laboriosam ratione animi iste optio, veritatis nihil amet cupiditate a natus vitae! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem non a odit veritatis nam quae animi eos recusandae doloremque aut, id enim? Quod sit minima unde nemo temporibus in sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis quidem, perferendis nam velit porro, harum rerum, ipsa ut possimus aperiam eaque assumenda eum ducimus ab fuga. Est magnam velit laudantium!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="bg-dark tinggi-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center text-white">&copy; Copyright 2018 Designed By <a href="">Abu T.A</a></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <button id="topBtn"><i class="fas fa-arrow-up"></i></button>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
